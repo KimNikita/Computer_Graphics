0b29cfd0e4c4d50f823b7a60ccfecaa5fdfff167
KimNikita <ghj710@mail.ru>
2021-03-11 12:33:45 +0300

    Переделал фильтры краев

---
+++ Lab_1/Task_1/SobelFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+  class SobelFilter : Filters
+  {
+    SobelXFilter filter1;
+    SobelYFilter filter2;
+
+    public SobelFilter()
+    {
+      filter1 = new SobelXFilter();
+      filter2 = new SobelYFilter();
+    }
+
+    internal override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+    {
+      Color color1 = filter1.calculateNewPixelColor(sourceImage, x, y);
+      Color color2 = filter2.calculateNewPixelColor(sourceImage, x, y);
+      return Color.FromArgb(Clamp(Math.Abs(color1.R) + Math.Abs(color2.R), 0, 255), Clamp(Math.Abs(color1.G) + Math.Abs(color2.G), 0, 255), Clamp(Math.Abs(color1.B) + Math.Abs(color2.B), 0, 255));
+    }
+  }
+}
