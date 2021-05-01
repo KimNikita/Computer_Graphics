d17c83ba008f4a166fb5dd6a10ba0486c65fe419
KimNikita <ghj710@mail.ru>
2021-05-01 16:34:27 +0300

    begin

---
+++ Lab_3/Laba_3/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace Laba_3
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.openGlControl = new OpenTK.GLControl();
+      this.SuspendLayout();
+      // 
+      // openGlControl
+      // 
+      this.openGlControl.BackColor = System.Drawing.Color.Black;
+      this.openGlControl.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.openGlControl.Location = new System.Drawing.Point(0, 0);
+      this.openGlControl.Margin = new System.Windows.Forms.Padding(1);
+      this.openGlControl.Name = "openGlControl";
+      this.openGlControl.RightToLeft = System.Windows.Forms.RightToLeft.No;
+      this.openGlControl.Size = new System.Drawing.Size(384, 361);
+      this.openGlControl.TabIndex = 0;
+      this.openGlControl.VSync = false;
+      this.openGlControl.Load += new System.EventHandler(this.openGlControl_Load);
+      this.openGlControl.Paint += new System.Windows.Forms.PaintEventHandler(this.openGlControl_Paint);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(384, 361);
+      this.Controls.Add(this.openGlControl);
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Raytracing";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private OpenTK.GLControl openGlControl;
+  }
+}
+
---
+++ Lab_3/Laba_3/Form1.cs
@@ -0,0 +1,109 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba_3
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    int RayTracingProgramID;
+    int RayTracingVertexShader;
+    int RayTracingFragmentShader;
+
+    //private static void Resize(int width, int height)
+    //{
+    //  if (height == 0)
+    //  {
+    //    height = 1;
+    //  }
+    //  GL.Viewport(0, 0, width, height);
+    //}
+
+    private static bool Init()
+    {
+      GL.Enable(EnableCap.ColorMaterial);
+      GL.ShadeModel(ShadingModel.Smooth);
+
+      GL.Enable(EnableCap.DepthTest);
+      GL.Enable(EnableCap.CullFace);
+
+      GL.Enable(EnableCap.Lighting);
+      GL.Enable(EnableCap.Light0);
+
+      GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+
+      return true;
+    }
+
+    private bool InitShaders()
+    {
+      RayTracingProgramID = GL.CreateProgram();
+      loadShader("..\\..\\Shaders\\raytracing.vert", ShaderType.VertexShader, RayTracingProgramID, out RayTracingVertexShader);
+      loadShader("..\\..\\Shaders\\raytracing.frag", ShaderType.FragmentShader, RayTracingProgramID, out RayTracingFragmentShader);
+
+      GL.LinkProgram(RayTracingProgramID);
+      Console.WriteLine(GL.GetProgramInfoLog(RayTracingProgramID));
+      GL.Enable(EnableCap.Texture2D);
+      return true;
+    }
+
+    void loadShader(String filename, ShaderType type, int program, out int address)
+    {
+      address = GL.CreateShader(type);
+      using (System.IO.StreamReader sr = new StreamReader(filename))
+      {
+        GL.ShaderSource(address, sr.ReadToEnd());
+      }
+      GL.CompileShader(address);
+      GL.AttachShader(program, address);
+      Console.WriteLine(GL.GetShaderInfoLog(address));
+    }
+
+    private void Draw()
+    {
+      GL.ClearColor(Color.AliceBlue);
+      GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+      GL.UseProgram(RayTracingProgramID);
+
+      openGlControl.SwapBuffers();
+      GL.UseProgram(0);
+    }
+
+    private void openGlControl_Load(object sender, EventArgs e)
+    {
+      Draw();
+    }
+
+    private void openGlControl_Paint(object sender, PaintEventArgs e)
+    {
+      Init();
+      //Resize(openGlControl.Width, openGlControl.Height);
+      InitShaders();
+    }
+
+    //private void openGlControl_Resize(object sender, EventArgs e)
+    //{
+    //  Resize(openGlControl.Width, openGlControl.Height);
+    //}
+
+    //private void Form1_ClientSizeChanged(object sender, EventArgs e)
+    //{
+    //  openGlControl.Width = this.ClientRectangle.Width - 24;
+    //  openGlControl.Height = this.ClientRectangle.Height - 24;
+    //}
+  }
+}
